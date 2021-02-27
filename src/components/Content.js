37c8f1deb93cb2478ab054a39082e1bedaa7f8ed
Jimmy Castex <jim.castex@gmail.com>
2021-02-27 13:16:52 +0100

    fixed scrolling issue

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 
 export default function Content(props) {
+  const contentRef = useRef(null);
+
+  useEffect(() => {
+    if (contentRef.current) {
+      // reset scrolling when changing tab
+      contentRef.current.scrollTo(0, 0);
+    }
+  });
+
   return (
-    <div className="content">
+    <div ref={contentRef} className="content">
       {props.children}
     </div>
   );
