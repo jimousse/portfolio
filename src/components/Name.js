ea686c82ebc043eb373fbd048eed556107314bff
Jimmy Castex <jim.castex@gmail.com>
2021-02-28 20:46:29 +0100

    name change

--- src/components/Name.js
+++ src/components/Name.js
@@ -4,7 +4,7 @@ export default function Name() {
   return (
     <div className="name">
       <h1 className="heading-1 name__name">Jimmy Castex</h1>
-      <span className="name__job">Frontend developer</span>
+      <span className="name__job">Software developer</span>
     </div>
   );
 }
