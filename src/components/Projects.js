b97bdb4b680f9e2ac36e4d9c9160aa867466bb8b
jimmy.castex <jimmy.castex@servicenow.com>
2021-04-08 16:06:59 +0200

    added topics for projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,7 +5,7 @@ const BLOCK_NAME = 'project';
 export default function Projects() {
 	return (
 		<div className="projects">
-			{projectsData.map(({ title, description, img, github, demo }, i) => {
+			{projectsData.map(({ title, description, img, github, demo, topics }, i) => {
 				return (
 					<div key={i} className={`${BLOCK_NAME}__card`}>
 						<div
@@ -14,6 +14,17 @@ export default function Projects() {
 						/>
 						<h5 className={`${BLOCK_NAME}__title`}>{title}</h5>
 						<div className={`${BLOCK_NAME}__description`}>{description}</div>
+						{topics &&
+							<div className={`${BLOCK_NAME}__topics`}>
+							{topics.map(topic => {
+								return (
+									<span className={`${BLOCK_NAME}__topic`}>
+										{topic}
+									</span>
+								);
+							})}
+							</div>
+						}
 						<div className={`${BLOCK_NAME}__links`}>
 							<a className={`${BLOCK_NAME}__link ${BLOCK_NAME}__link--github`} href={github} />
 							{demo &&
