23bae171408c7eae21cc176e927d1f39df4b06ef
Jimmy Castex <jim.castex@gmail.com>
2021-10-15 17:13:36 +0200

    new project

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,40 +1,52 @@
 export const projectsData = [
   {
-    title: 'Beer O\'Clock',
+    title: 'This portofolio',
+    description:
+      'Website you are currently looking at. Built with React and Sass only.',
+    img: 'https://raw.githubusercontent.com/jimousse/portfolio/main/img/projects/portfolio.png',
+    github: 'https://github.com/jimousse/portfolio',
+    topics: ['reactjs', 'sass', 'webpack'],
+  },
+  {
+    title: 'Login page',
+    description: 'Login project done in React using hooks and context.',
+    img: 'https://raw.githubusercontent.com/jimousse/portfolio/main/img/projects/login.png',
+    github: 'https://github.com/jimousse/login-react-app',
+    demo: 'https://jimousse.github.io/login-react-app/',
+    topics: ['reactjs', 'hooks', 'context'],
+  },
+  {
+    title: "Beer O'Clock",
     description: 'Funny project, playing with CSS animations and SVG.',
     img: 'https://raw.githubusercontent.com/jimousse/portfolio/main/img/projects/beer.png',
     github: 'https://github.com/jimousse/beer-oclock',
     demo: 'https://jimousse.github.io/beer-oclock/',
-    topics: [ 'svg', 'css', 'animations' ]
+    topics: ['svg', 'reactjs', 'animations'],
   },
-  {
-    title: 'This portofolio',
-    description: 'Website you are currently looking at. Built with React and Sass only.',
-    img: 'https://raw.githubusercontent.com/jimousse/portfolio/main/img/projects/portfolio.png',
-    github: 'https://github.com/jimousse/portfolio',
-    topics: [ 'reactjs', 'sass', 'webpack' ]
-  },
+
   {
     title: '2D Game',
-    description: 'This is a tile-based game concept using HTML5 <canvas>. Built with JavaScript and LitElement.',
+    description:
+      'This is a tile-based game concept using HTML5 <canvas>. Built with JavaScript and LitElement.',
     img: 'https://raw.githubusercontent.com/jimousse/portfolio/main/img/projects/2dgame.png',
     github: 'https://github.com/jimousse/2DGame',
     demo: 'https://jimousse.github.io/2DGame/',
-    topics: [ 'javascript', 'canvas', 'lit-element' ]
+    topics: ['javascript', 'canvas', 'lit-element'],
   },
   {
     title: 'Book Shop',
     description: 'Full stack book shop website made with NodeJS and MongoDB.',
     img: 'https://raw.githubusercontent.com/jimousse/portfolio/main/img/projects/node-shop.jpg',
     github: 'https://github.com/jimousse/node-mongodb-shop',
-    topics: [ 'nodejs', 'mongodb', 'expressjs' ]
+    topics: ['nodejs', 'mongodb', 'expressjs'],
   },
   {
     title: 'Social Media REST API',
-    description: 'Node REST API for a social media client created with Express, MongoDB/Mongoose.',
+    description:
+      'Node REST API for a social media client created with Express, MongoDB/Mongoose.',
     img: 'https://raw.githubusercontent.com/jimousse/portfolio/main/img/projects/rest-api.png',
     github: 'https://github.com/jimousse/node-rest-api',
-    topics: [ 'rest-api', 'mongoose', 'nodejs' ]
+    topics: ['rest-api', 'mongoose', 'nodejs'],
   },
   {
     title: 'Hotel Domstraat',
@@ -42,7 +54,7 @@ export const projectsData = [
     img: 'https://raw.githubusercontent.com/jimousse/portfolio/main/img/projects/hotel-app.png',
     github: 'https://github.com/jimousse/hotel-app',
     demo: 'https://jimousse.github.io/hotel-app/',
-    topics: [ 'css', 'sass', 'css-flexbox' ]
+    topics: ['css', 'sass', 'css-flexbox'],
   },
   {
     title: 'Nature Tours App',
@@ -50,7 +62,7 @@ export const projectsData = [
     img: 'https://raw.githubusercontent.com/jimousse/portfolio/main/img/projects/natours.png',
     github: 'https://github.com/jimousse/nature-tour-app',
     demo: 'https://jimousse.github.io/nature-tour-app/',
-    topics: [ 'css', 'sass', 'float' ]
+    topics: ['css', 'sass', 'float'],
   },
   {
     title: 'Real Estate App',
@@ -58,6 +70,6 @@ export const projectsData = [
     img: 'https://raw.githubusercontent.com/jimousse/portfolio/main/img/projects/real-estate.png',
     demo: 'https://jimousse.github.io/real-estate-app/',
     github: 'https://github.com/jimousse/real-estate-app',
-    topics: [ 'css', 'sass', 'css-grid' ]
-  }
+    topics: ['css', 'sass', 'css-grid'],
+  },
 ];
