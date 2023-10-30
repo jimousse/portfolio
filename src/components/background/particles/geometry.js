3169a309a41ce193681300595d24f2d80dfd3b70
Jimmy <jim.castex@gmail.com>
2023-10-30 12:30:02 -0700

    andrei's feedback

--- src/components/background/particles/geometry.js
+++ src/components/background/particles/geometry.js
@@ -1,6 +1,6 @@
-import * as THREE from 'three';
+import * as THREE from "three";
 
-const COUNT = 10000;
+const COUNT = 2000;
 
 const positions = new Float32Array(COUNT * 3);
 for (let i = 0; i < COUNT * 3; i++) {
@@ -16,10 +16,10 @@ for (let i = 0; i < COUNT; i++) {
 
 const particlesGeometry = new THREE.BufferGeometry();
 particlesGeometry.setAttribute(
-  'position',
+  "position",
   new THREE.BufferAttribute(positions, 3)
 );
 
-particlesGeometry.setAttribute('color', new THREE.BufferAttribute(colors, 3));
+particlesGeometry.setAttribute("color", new THREE.BufferAttribute(colors, 3));
 
 export { particlesGeometry };
